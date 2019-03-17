d85538f7fca5c285d07dab487545362122a1f025
Yurij2015 <nabor_2008@i.ua>
2019-03-17 11:15:53 +0300

    dropdownlist category

--- views/photodoc/_form.php
+++ views/photodoc/_form.php
@@ -3,6 +3,8 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use dosamigos\tinymce\TinyMce;
+use app\models\Category;
+use yii\helpers\ArrayHelper;
 
 
 /* @var $this yii\web\View */
@@ -14,16 +16,16 @@ use dosamigos\tinymce\TinyMce;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'user_iduser')->textInput() ?>
+    <!--    --><? //= $form->field($model, 'user_iduser')->textInput() ?>
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
 
-<!--    --><?//= $form->field($model, 'doclink')->textInput(['maxlength' => true]) ?>
+    <!--    --><? //= $form->field($model, 'doclink')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'doclink')->widget(TinyMce::className(), [
-        'options' => ['rows' => 6],
+        'options' => ['rows' => 20],
         'language' => 'ru',
         'clientOptions' => [
             'plugins' => [
@@ -33,11 +35,17 @@ use dosamigos\tinymce\TinyMce;
             ],
             'toolbar' => "undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image"
         ]
-    ]);?>
+    ]); ?>
 
     <?= $form->field($model, 'dateadd')->textInput() ?>
 
-    <?= $form->field($model, 'category_idcategory')->textInput() ?>
+    <!--https://p0vidl0.info/yii2-vypadayushhij-spisok-dropdownlist.html-->
+    <?php $category = Category::find()->all();
+    // формируем массив, с ключем равным полю 'idcategory' и значением равным полю 'categoryname'
+    $items = ArrayHelper::map($category, 'idcategory', 'categoryname');
+    $params = ['prompt' => 'Выберите категорию']; ?>
+    <?= $form->field($model, 'category_idcategory')->dropDownList($items, $params); ?>
+    <!--    --><? //= $form->field($model, 'category_idcategory')->textInput() ?>
 
     <?= $form->field($model, 'doc_file')->textInput() ?>
 
